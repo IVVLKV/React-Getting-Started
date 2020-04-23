64779eb45dfab4acdc85700b4b2398afdd53773e
IVVLKV <32835613+IVVLKV@users.noreply.github.com>
2020-04-23 18:14:03 +0300

    Course Completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./styles.css";
 
-import Button from "./Button/Button";
+import ButtonComponent from "./ButtonComponent/ButtonComponent";
 import GitHubCards from "./ClassComponent/";
+import StarMatch from "./StarGame/StarGame";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Components Demos:</h1>
       <div style={{ background: "#ddd", padding: "20px" }}>
-        <Button />
+        <ButtonComponent />
+      </div>
+
+      <div style={{ background: "#eee", padding: "20px" }}>
+        <GitHubCards title={"GitHub Cards!"} />
       </div>
 
       <div style={{ background: "#fff", padding: "20px" }}>
-        <GitHubCards title={"GitHub Cards!"} />
+        <StarMatch />
       </div>
     </div>
   );
